684776a7633d1589b151825cd7144b4458ba96a0
sudipdholariya <sudipdholariya8@gmail.com>
2024-10-22 11:55:26 +0530

    first commit

---
+++ src/componet/Firstpage.js
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+function Firstpage() {
+  return (
+    <>
+    <Box sx={{backgroundImage:"url(https://climateextremes.org.au/wp-content/uploads/2019/01/Sunny-Melbourne-pixabay.jpg)",height:"800px",backgroundSize:"cover",position:"sticky"}}>
+
+    </Box>
+    </>
+  )
+}
+
+export default Firstpage
